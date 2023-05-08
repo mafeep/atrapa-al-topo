a50b6e636f939b59d004bba1949ae6b3e173abf5
mafeep <123569644+mafeep@users.noreply.github.com>
2023-05-08 11:59:33 -0500

    ola si

---
+++ app.js
@@ -0,0 +1,6 @@
+const cuadrado = document.querySelectorAll(".cuadrado");
+const tiempofaltante = document.querySelectorAll("#tiempo");
+let puntaje = document.getElementById("puntaje");
+
+let resultado = 0;
+let tiempoactual = tiempofaltante.textContent;
